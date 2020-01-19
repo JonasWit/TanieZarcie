4d26d6f25826badae093734a339b63022d2d3312
Viers <jonaszwitek@gmail.com>
2020-01-19 11:28:41 +0100

    e3

--- WEB.Shop.Application/Products/CreateProduct.cs
+++ WEB.Shop.Application/Products/CreateProduct.cs
@@ -1,11 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using WEB.Shop.DataBase;
+using WEB.Shop.Domain.Models;
 
 namespace WEB.Shop.Application.Products
 {
     public class CreateProduct
     {
+        private ApplicationDbContext _context;
+
+        public CreateProduct(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Do(int id, string name, string description)
+        {
+            _context.Products.Add(new Product { Id = id, Name = name, Description = description });
+
+
+        }
+
+
+
+
+
 
     }
 }
