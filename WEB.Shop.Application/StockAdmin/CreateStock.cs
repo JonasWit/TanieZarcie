9b92976e4adc6571b188f610f10161f938f20e49
Viers <jonaszwitek@gmail.com>
2020-02-01 10:46:30 +0100

    e15

---
+++ WEB.Shop.Application/StockAdmin/CreateStock.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WEB.Shop.DataBase;
+using WEB.Shop.Domain.Models;
+
+namespace WEB.Shop.Application.StockAdmin
+{
+    public class CreateStock
+    {
+        private ApplicationDbContext _context;
+
+        public CreateStock(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Response> Do(Request request)
+        {
+            var stock = new Stock
+            {
+                Description = request.Description,
+                Quantity = request.Quantity,
+                ProductId = request.ProductId
+            };
+
+            _context.Stock.Add(stock);
+            await _context.SaveChangesAsync();
+
+            return new Response
+            {
+                Id = stock.Id,
+                Description = stock.Description,
+                Quantity = stock.Quantity
+            };
+        }
+
+        public class Request
+        {
+            public int ProductId { get; set; }
+            public string Description { get; set; }
+            public int Quantity { get; set; }
+        }
+
+        public class Response
+        {
+            public int Id { get; set; }
+            public string Description { get; set; }
+            public int Quantity { get; set; }
+        }
+    }
+}
---
+++ WEB.Shop.Application/StockAdmin/GetStocks.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WEB.Shop.DataBase;
+
+namespace WEB.Shop.Application.StockAdmin
+{
+    public class GetStocks
+    {
+        private ApplicationDbContext _context;
+
+        public GetStocks(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<StockViewModel> Do(int productId)
+        {
+            var stock = _context.Stock
+                .Where(x => x.ProductId == productId)
+                .Select(x => new StockViewModel 
+                { 
+                    Id = x.Id,
+                    Description = x.Description,
+                    Quantity = x.Quantity
+                })
+                .ToList();
+
+
+            return stock;
+        }
+
+        public class StockViewModel
+        {
+            public int Id { get; set; }
+            public int ProductId { get; set; }
+            public string Description { get; set; }
+            public int Quantity { get; set; }
+        }
+
+    }
+}
