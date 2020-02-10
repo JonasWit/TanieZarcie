07e833e9173f0ee64a2734c13526d3fd5aaa9b70
Viers <jonaszwitek@gmail.com>
2020-02-10 20:20:17 +0100

    e21

---
+++ WEB.Shop.UI/ViewComponents/CartViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WEB.Shop.Application.Cart;
+using WEB.Shop.DataBase;
+
+namespace WEB.Shop.UI.ViewComponents
+{
+    public class CartViewComponent : ViewComponent
+    {
+        private ApplicationDbContext _context;
+
+        public CartViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke(string view = "Default")
+        {  
+            return View(view, new GetCart(HttpContext.Session, _context).Do());
+        }
+    }
+}
