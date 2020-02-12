9dea662a50e2f0be4f4537651b921f24d7bdab75
Viers <jonaszwitek@gmail.com>
2020-02-12 23:14:26 +0100

    e28p1

---
+++ WEB.Shop.Domain/Models/StockOnHold.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WEB.Shop.Domain.Models
+{
+    public class StockOnHold
+    {
+        public int Id { get; set; }
+    }
+}
