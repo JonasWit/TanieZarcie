c3980eb36149e63338313a02fcc5c2eb9c9f4558
Viers <jonaszwitek@gmail.com>
2020-03-10 15:47:33 +0100

    crawlers database wip

--- WEB.Shop.Application/Crawlers/CrawlersCommander.cs
+++ WEB.Shop.Application/Crawlers/CrawlersCommander.cs
@@ -1,35 +1,27 @@
 ﻿using System.Collections.Generic;
 using WEB.SearchEngine;
+using WEB.Shop.Domain.Infrastructure;
 using WEB.Shop.Domain.Models;
 
 namespace WEB.Shop.Application.Crawlers
 {
     public class CrawlersCommander
     {
-        public Engine SearchEngine { get; private set; }
+        private ICrawlersDataBaseManager _crawlersDataBaseManager;
+        private Engine _searchEngine;
+
         public List<Product> DomainModels { get; private set; }
         public List<SearchEngine.SearchResultsModels.Product> EngineModels { get; private set; }
 
-        public int test { get; set; } = 2;
-
-        public void Add()
+        public CrawlersCommander(ICrawlersDataBaseManager crawlersDataBaseManager)
         {
-            test++;
-        }
-
-        public void Remove()
-        {
-            test--;
-        }
-
-        public CrawlersCommander()
-        {
-            SearchEngine = new Engine();
+            _crawlersDataBaseManager = crawlersDataBaseManager;
+            _searchEngine = new Engine();
         }
 
         public bool RunEngine()
         {
-            SearchEngine.RunCrawlerForBiedronkaAsync();
+            _searchEngine.RunCrawlerForBiedronkaAsync();
 
             return true;
 
--- WEB.Shop.DataBase/CrawlersDataBaseManager.cs
+++ WEB.Shop.DataBase/CrawlersDataBaseManager.cs
@@ -19,9 +19,36 @@ namespace WEB.Shop.DataBase
         public Task<int> RefreshDatabase(List<Product> products)
         {
 
+            //public Task<int> CreateProduct(Product product)
+            //{
+            //    _context.Products.Add(product);
+            //    return _context.SaveChangesAsync();
+            //}
 
 
- 
+            //public Task<int> CreateStock(Stock stock)
+            //{
+            //    _context.Stock.Add(stock);
+            //    return _context.SaveChangesAsync();
+            //}
+
+
+
+            //var stock = new Stock
+            //{
+            //    Description = request.Description,
+            //    Quantity = request.Quantity,
+            //    ProductId = request.ProductId
+            //};
+
+            //await _stockManager.CreateStock(stock);
+
+
+
+
+
+
+
             return _context.SaveChangesAsync();
         }
 
