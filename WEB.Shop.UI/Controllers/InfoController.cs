69a68f57d4cacde32e287883651e29107b954bf3
Viers <jonaszwitek@gmail.com>
2020-03-18 14:26:15 +0100

    info page

---
+++ WEB.Shop.UI/Controllers/InfoController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB.Shop.UI.Controllers
+{
+    public class InfoController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
