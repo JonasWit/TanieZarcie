286c2edeef5f777708d67e6f23f8ef53fb248eef
Viers <jonaszwitek@gmail.com>
2020-03-21 14:00:41 +0100

    cleanup

--- WEB.Shop.DataBase/ApplicationDbContext.cs
+++ WEB.Shop.DataBase/ApplicationDbContext.cs
@@ -1,9 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.SqlServer;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using WEB.Shop.Domain.Models;
 
 namespace WEB.Shop.DataBase
