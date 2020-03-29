320ff6f0e56f73d306f56a5ea90476609ccc0d75
Viers <jonaszwitek@gmail.com>
2020-03-29 20:01:12 +0200

    wip

---
+++ WEB.Shop.Application/ApplicationServices/ProductsService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WEB.Shop.Application.ApplicationServices
+{
+    [Service]
+    public class ProductsService
+    {
+
+
+    }
+}
