55a3d1d24edc3eab7920167ba1c1eeff8a64130f
Viers <jonaszwitek@gmail.com>
2020-04-01 10:59:26 +0200

    enums corrected and somehtml in admin panel

---
+++ WEB.Shop.Application/Enums/ShopEnums.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WEB.Shop.Application.Enums
+{
+    public enum Shops
+    {
+        Biedronka = 0,
+        Lidl = 1,
+        Kaufland = 2,
+        Carrefour = 3,
+        Auchan = 4,
+        Stokrotka = 5,
+        Zabka = 6
+    }
+}
