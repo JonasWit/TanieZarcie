bd91b83873b7f0200f20b28b6380c49da78dd140
Viers <jonaszwitek@gmail.com>
2020-04-07 12:47:53 +0200

    files manager updated

--- WEB.Shop.DataBase/FileManager.cs
+++ WEB.Shop.DataBase/FileManager.cs
@@ -1,25 +1,37 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 using WEB.Shop.Domain.Infrastructure;
 
 namespace WEB.Shop.DataBase
 {
+    public enum ContentStore
+    { 
+        News = 0,
+        Carousel = 1,
+    }
+
     public class FileManager : IFileManager
     {
-        private readonly string _imagePath;
+        private readonly string _newsImagesPath;
+        private readonly string _carouselImagesPath;
 
-        public FileManager(IConfiguration configuration) => _imagePath = configuration["Path:Images"];
+        public FileManager(IConfiguration configuration)
+        { 
+            _newsImagesPath = configuration["Path:News"];
+            _carouselImagesPath = configuration["Path:Carousel"];
+        } 
 
-        public FileStream ImageStream(string image) => new FileStream(Path.Combine(_imagePath, image), FileMode.Open, FileAccess.Read);
+        public FileStream ImageStream(string image) => new FileStream(Path.Combine(_newsImagesPath, image), FileMode.Open, FileAccess.Read);
 
         public async Task<string> SaveImageAsync(IFormFile image)
         {
             try
             {
-                var savePath = Path.Combine(_imagePath);
+                var savePath = Path.Combine(_newsImagesPath);
 
                 if (!Directory.Exists(savePath)) Directory.CreateDirectory(savePath);
 
@@ -36,11 +48,32 @@ namespace WEB.Shop.DataBase
             }
         }
 
+        public List<string> GetAllPicturesFromContent(ContentStore contentSubfolder)
+        {
+            var reult = new List<string>();
+
+            
+
+            switch (contentSubfolder)
+            {
+                case ContentStore.News:
+                    Directory.GetFiles(_newsImagesPath);
+                    break;
+                case ContentStore.Carousel:
+                    Directory.GetFiles(_carouselImagesPath);
+                    break;
+                default:
+                    break;
+            }
+
+            return reult;
+        }
+
         public bool DeleteImage(string image)
         {
             try
             {
-                var file = Path.Combine(_imagePath, image);
+                var file = Path.Combine(_newsImagesPath, image);
 
                 if (File.Exists(file)) File.Delete(file);
 
