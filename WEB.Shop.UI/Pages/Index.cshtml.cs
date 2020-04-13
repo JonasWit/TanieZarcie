6bb11d19bf3fbd3fc448af35c653fc4a36dcb0b2
Viers <jonaszwitek@gmail.com>
2020-04-13 21:42:55 +0200

    css wip

--- WEB.Shop.UI/Pages/Index.cshtml.cs
+++ WEB.Shop.UI/Pages/Index.cshtml.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using WEB.Shop.Application.Enums;
 using WEB.Shop.Application.Products;
 using WEB.Shop.Application.Session;
@@ -14,20 +13,22 @@ namespace WEB.Shop.UI.Pages
         [BindProperty]
         public string ShopDirection { get; set; }
 
-        public Dictionary<Shops, int> Products { get; set; }
-        public Dictionary<Shops, int> ProductsOnSale { get; set; }
+        public Dictionary<string, int> Products { get; set; }
+        public Dictionary<string, int> ProductsOnSale { get; set; }
 
         public List<string> Shops { get; set; }
 
         public void OnGet([FromServices] GetProducts getProducts)
         {
-            Products = new Dictionary<Shops, int>();
-            ProductsOnSale = new Dictionary<Shops, int>();
+            Products = new Dictionary<string, int>();
+            ProductsOnSale = new Dictionary<string, int>();
+            Shops = new List<string>();
 
             foreach (Shops shop in (Shops[])Enum.GetValues(typeof(Shops)))
             {
-                Products.Add(shop, getProducts.CountProductForShop(shop.ToString()));
-                ProductsOnSale.Add(shop, getProducts.CountProductOnSaleForShop(shop.ToString()));
+                Products.Add(shop.ToString(), getProducts.CountProductForShop(shop.ToString()));
+                ProductsOnSale.Add(shop.ToString(), getProducts.CountProductOnSaleForShop(shop.ToString()));
+                Shops.Add(shop.ToString());
             }
         }
 
@@ -37,31 +38,31 @@ namespace WEB.Shop.UI.Pages
             return RedirectToPage("MainProducts");
         }
 
-        public IActionResult OnPostBiedronka([FromServices] SaveSelectedShop saveSelectedShop)
+        public IActionResult OnGetBiedronka([FromServices] SaveSelectedShop saveSelectedShop)
         {
             saveSelectedShop.Do("Biedronka");
             return RedirectToPage("MainProducts");
         }
 
-        public IActionResult OnPostLidl([FromServices] SaveSelectedShop saveSelectedShop)
+        public IActionResult OnGetLidl([FromServices] SaveSelectedShop saveSelectedShop)
         {
             saveSelectedShop.Do("Lidl");
             return RedirectToPage("MainProducts");
         }
 
-        public IActionResult OnPostKaufland([FromServices] SaveSelectedShop saveSelectedShop)
+        public IActionResult OnGetKaufland([FromServices] SaveSelectedShop saveSelectedShop)
         {
             saveSelectedShop.Do("Kaufland");
             return RedirectToPage("MainProducts");
         }
 
-        public IActionResult OnPostCarrefour([FromServices] SaveSelectedShop saveSelectedShop)
+        public IActionResult OnGetCarrefour([FromServices] SaveSelectedShop saveSelectedShop)
         {
             saveSelectedShop.Do("Carrefour");
             return RedirectToPage("MainProducts");
         }
 
-        public IActionResult OnPostAuchan([FromServices] SaveSelectedShop saveSelectedShop)
+        public IActionResult OnGetAuchan([FromServices] SaveSelectedShop saveSelectedShop)
         {
             saveSelectedShop.Do("Auchan");
             return RedirectToPage("MainProducts");
