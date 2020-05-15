7ae01259b2aa91f459d7b3227f2864870d48c53d
Viers <jonaszwitek@gmail.com>
2020-05-15 17:47:05 +0200

    css wip

--- WEB.Shop.UI/wwwroot/js/admin/adminStyleScripts.js
+++ WEB.Shop.UI/wwwroot/js/admin/adminStyleScripts.js
@@ -1,1 +1,16 @@
-﻿
+﻿function activateSideBarAdminButton(buttonId) {
+    var buttons = document.querySelectorAll('[id ^= "sidebar-button-"]');
+
+    for (var i = 0; i < buttons.length; i++) {
+        if (buttonId === buttons[i].id) {
+            if (!buttons[i].classList.contains('tza-current')) {
+                buttons[i].classList.add('tza-current');
+            }  
+        }
+        else {
+            if (buttons[i].classList.contains('tza-current')) {
+                buttons[i].classList.remove('tza-current');
+            }   
+        }
+    }      
+}
