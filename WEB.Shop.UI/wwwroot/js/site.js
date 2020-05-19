cc29d39e155d72695342709fbe54aaf950e72e10
Viers <jonaszwitek@gmail.com>
2020-05-19 23:29:09 +0200

    New cards on index

--- WEB.Shop.UI/wwwroot/js/site.js
+++ WEB.Shop.UI/wwwroot/js/site.js
@@ -28,4 +28,27 @@ document.addEventListener('DOMContentLoaded', () => {
 
 });
 
+$(document).ready(function () {
+
+    var stack1 = $('#stack1');
+
+    stack1.removeClass('start');
+
+    stack1.hammer().on('tap', function (event) {
+        stack1.addClass('hover');
+        event.stopPropagation();
+    });
+
+
+    $(document).hammer().on('tap', function (event) {
+        stack1.removeClass('hover');
+        $('.card').removeClass('hover');
+    });
+
+    $('.card').hammer().on('tap', function (event) {
+        $('.card').removeClass('hover');
+        $(this).addClass('hover');
+    });
+});
+
 
