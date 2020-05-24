9e76824db6bc0a3cc2cb08476902f631ed7a88d3
Viers <jonaszwitek@gmail.com>
2020-05-24 22:58:10 +0200

    Index layout - footer clean-up; scroll down & scroll top added. Prodcut overview started.

--- WEB.Shop.UI/wwwroot/js/site.js
+++ WEB.Shop.UI/wwwroot/js/site.js
@@ -28,26 +28,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
 });
 
-$(document).ready(function () {
 
-    var stack1 = $('#stack1');
 
-    stack1.hammer().on('tap', function (event) {
-        stack1.addClass('hover');
-        event.stopPropagation();
-    });
-
-    $(document).hammer().on('tap', function (event) {
-        stack1.removeClass('hover');
-        $('.card-front').removeClass('hover');
-    });
-
-    $('.card-front').hammer().on('tap', function (event) {
-        $('.card-front').removeClass('hover');
-        $(this).addClass('hover');
-    });
-});
-
+//Cards
 $(document).ready(function () {
 
     var stack1 = $('#stack1');
@@ -62,11 +45,38 @@ $(document).ready(function () {
 
     $(document).hammer().on('tap', function (event) {
         stack1.removeClass('hover');
-        $('.card').removeClass('hover');
+        $('.card-index').removeClass('hover');
     });
 
-    $('.card').hammer().on('tap', function (event) {
-        $('.card').removeClass('hover');
+    $('.card-index').hammer().on('tap', function (event) {
+        $('.card-index').removeClass('hover');
         $(this).addClass('hover');
     });
 });
+
+
+// Scroll down:
+function scrollWin() {
+    window.scrollBy(0, 900);
+}
+
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
